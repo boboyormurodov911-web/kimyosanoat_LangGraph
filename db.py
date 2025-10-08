376bc36c6269b45f5a5c528fa4c4f00950e91fd4
Bekzod Alimukhamedov <b.alimuhamedov@uzdc.uz>
2025-10-08 11:09:33 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /db.py

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ import psycopg2.extras
 import os
 
 
-DB_HOST = os.getenv("DB_HOST", "192.168.1.7")
-DB_NAME = os.getenv("DB_NAME", "aidb2")
-DB_USER = os.getenv("DB_USER", "ai-user")
-DB_PASS = os.getenv("DB_PASS", "63D9WFhW4S4GQOXaPkyj")
+DB_HOST = os.getenv("DB_HOST", "192.168.1.24")
+DB_NAME = os.getenv("DB_NAME", "kimyosanoataidb")
+DB_USER = os.getenv("DB_USER", "kimyosanoatai-user")
+DB_PASS = os.getenv("DB_PASS", "39uWfFEy4qt9orC0MuuJ")
 
 def get_connection():
     return psycopg2.connect(
