7349a1a4af30459e68020214b22fe4d30d28e634
Javoxir Mamatayev <boboyormurodov911@gmail.com>
2025-10-08 18:08:25 +0000

    Edit create_table.py

--- create_table.py
+++ create_table.py
@@ -1,7 +1,5 @@
 import psycopg2
 
-
-
 conn = psycopg2.connect(
     dbname="kimyosanoataidb",
     user="kimyosanoatai-user",
@@ -10,39 +8,39 @@ conn = psycopg2.connect(
     port=5432,
 )
 
-
 def create_tables():
-
     cur = conn.cursor()
 
     # --- javoblar ---
     cur.execute("""
-                CREATE TABLE IF NOT EXISTS javoblar (
-                    id SERIAL PRIMARY KEY,
-                    number_of_answer INT
-                );
-            """)
-    
+        CREATE TABLE IF NOT EXISTS javoblar (
+            id SERIAL PRIMARY KEY,
+            number_of_answer INT
+        );
+    """)
+
+    # 🔹 faqat id=1 mavjud bo‘lmasa qo‘shadi
     cur.execute("""
-            INSERT INTO javoblar (number_of_answer) VALUES (0);
-        """)
+        INSERT INTO javoblar (id, number_of_answer)
+        VALUES (1, 0)
+        ON CONFLICT (id) DO NOTHING;
+    """)
 
-
-    #--- chat history ---
+    # --- chat_history ---
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS chat_history (
-    id SERIAL PRIMARY KEY,
-    session_id VARCHAR(255) NOT NULL,
-    user_question TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);""")
-
+        CREATE TABLE IF NOT EXISTS chat_history (
+            id SERIAL PRIMARY KEY,
+            session_id VARCHAR(255) NOT NULL,
+            user_question TEXT NOT NULL,
+            assistant_answer TEXT,
+            created_at TIMESTAMP DEFAULT NOW()
+        );
+    """)
 
     conn.commit()
     cur.close()
     conn.close()
     print("✅ Barcha jadvallar yaratildi!")
-
 
 if __name__ == "__main__":
     create_tables()
