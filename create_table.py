9f4bc34097471a248f3b24c1c2ad2a4fca9ce4be
boboyormurodov911-web <boboyormuradov911@gmail.com>
2025-10-08 12:57:46 +0500

    create_table fix

--- create_table.py
+++ create_table.py
@@ -1,30 +1,39 @@
 import psycopg2
 
-try:
-    conn = psycopg2.connect(
-        dbname="aidb2",
-        user="ai-user",
-        password="63D9WFhW4S4GQOXaPkyj",
-        host="192.168.1.7",
-        port=5432,
-    )
 
-    with conn.cursor() as cur:
-        # Jadval bo'lmasa yaratish
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS javoblar (
-                id SERIAL PRIMARY KEY,
-                number_of_answer INT
-            );
-        """)
-        # Bitta 0 qiymat qo'shish
-        cur.execute("""
+
+conn = psycopg2.connect(
+    dbname="aidb2",
+    user="ai-user",
+    password="63D9WFhW4S4GQOXaPkyj",
+    host="192.168.1.7",
+    port=5432,
+)
+
+
+def create_tables():
+
+    cur = conn.cursor()
+
+    # --- javoblar ---
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS javoblar (
+                    id SERIAL PRIMARY KEY,
+                    number_of_answer INT
+                );
+            """)
+    
+    cur.execute("""
             INSERT INTO javoblar (number_of_answer) VALUES (0);
         """)
-        conn.commit()
 
+
+
+    conn.commit()
+    cur.close()
     conn.close()
-    print("🔒 DB ulanishi yopildi.")
+    print("✅ Barcha jadvallar yaratildi!")
 
-except Exception as e:
-    print(f"❌ Xatolik yuz berdi: {e}")
+
+if __name__ == "__main__":
+    create_tables()
